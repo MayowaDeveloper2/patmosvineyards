20cd9d5a7165c4a6c21a8921e3066b307e4cfb11
MayowaDeveloper2 <akintoyemayowa1@gmail.com>
2024-10-12 12:29:12 +0100

    all task

--- src/app/components/herosection.tsx
+++ src/app/components/herosection.tsx
@@ -1,4 +1,5 @@
-'use client'
+/* eslint-disable react/no-unescaped-entities */
+'use client';
 
 import Image from "next/image";
 
--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -1,4 +1,5 @@
-'use client'
+/* eslint-disable react/no-unescaped-entities */
+'use client';
 
 import { useState } from "react";
 import Image from "next/image";
